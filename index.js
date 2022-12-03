b8d90f5c412da66e8efb78846e54d19c70f75520
eddiemuhoro <eddiemuhoro@gmail.com>
2022-12-03 21:59:51 +0300

    added dotenv

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use('/users', cors(), router)
 const port= process.env.PORT || 7000
 
 
-
+//app port ...
 
 app.listen(port, () => {
     console.log(`Server is running on port http://localhost:${port}`)
