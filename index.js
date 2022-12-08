41dfb6a68dbe3247453e5a1e9765144c69930cc0
eddiemuhoro <eddiemuhoro@gmail.com>
2022-12-08 12:34:06 +0300

    cc

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ app.use(cors(
     }
 ))
 
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//     next();
+// });
 //connect with database
 mongoose
 .connect(process.env.MONGO_URL)
